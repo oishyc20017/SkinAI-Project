1f4b6b109ee15dfc8ad72356e0b7122e726b9532
wishychakmaa <oishyc89@gmail.com>
2026-07-13 16:34:24 +0600

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,10 @@ import time
 import secrets
 import google.generativeai as genai
 from requests_oauthlib import OAuth2Session
+import os
+
+st.write("Current Working Directory:", os.getcwd())
+st.write("Database Path:", os.path.abspath("skinai_wishy_v30.db"))
 
 
 # ---------------- CONFIG ----------------
